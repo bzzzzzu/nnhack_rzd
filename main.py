ed8104d121f20a05e375083ed1d5c4b379d73cad
asdgg <a@a.a>
2023-09-08 23:32:29 +0300

    LLM для ответа пользователю, начальная версия

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ from tts_model import get_wav_from_text
 import gradio as gr
 
 def respond(text):
+    print(f'user question: {text}')
     embedding = get_embedding(text)
 
     # Поиск cosine simularity
@@ -29,8 +30,35 @@ def respond(text):
     for embed in embeddings_raw:
         score = cos(torch.Tensor(embedding), torch.Tensor(embed))
         scores.append(score[0])
-    print(scores)
+    #print(scores)
     sorted_index = np.argsort(scores)[::-1]
+
+    # запрос к LLM
+    # vicuna 1.1 as stated on a model card
+    llm_prompt = f'A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user\'s questions.\nUSER: '
+    # sub-instruction for specific task
+    llm_prompt = llm_prompt + 'Answer the question using only provided context below. Answer using your own understanding of the context. Be concise and precise. Answer in Russian.\n'
+    # User question
+    llm_prompt = llm_prompt + f'Question: {text}\n'
+    # Retrieval-Augmented Generation, 4 nearest examples from embedding database + solutions
+    llm_prompt = llm_prompt + 'Context: '
+    for i in range(0, 4):
+        #llm_prompt = llm_prompt + 'Issue: ' + embeddings_text[sorted_index[i]] + '.'
+        #llm_prompt = llm_prompt + 'Solution: ' + embeddings_answer[sorted_index[i]] + '.\n'
+        llm_prompt = llm_prompt + embeddings_answer[sorted_index[i]]
+
+    # LLM answer
+    llm_prompt = str.replace(llm_prompt, '..', '.')
+    llm_prompt = llm_prompt + '\nASSISTANT: '
+
+    full_llm_answer = get_reply(llm_prompt)
+    print(f'llm answer before prune: {full_llm_answer}\n')
+
+    #llm_prompt = llm_prompt + str(len(llm_prompt))
+    full_llm_answer = full_llm_answer[len(llm_prompt):]
+    print(f'llm answer after prune: {full_llm_answer}')
+    print('------------')
+    #full_llm_answer = full_llm_answer + str(len(full_llm_answer))
 
     solution_text = ''
     answer_string = ''
@@ -38,19 +66,20 @@ def respond(text):
         solution_text = f'{solution_text}{embeddings_answer[sorted_index[i]]}'
         answer_string = f'{answer_string}{embeddings_text[sorted_index[i]]}'
 
-    return answer_string, text, solution_text
+    return answer_string, solution_text, full_llm_answer, llm_prompt
 
 with gr.Blocks() as demo:
     text = gr.Textbox(label="Запрос")
-    output = gr.Textbox(label="Проблема")
+    problem = gr.Textbox(label="Проблема")
     solution = gr.Textbox(label="Метод устранения", lines=4)
+    llm_answer = gr.Textbox(label="Ответ помощника", lines=7)
     raw_prompt = gr.Textbox(label="Raw Prompt", lines=10)
 
-    text.submit(fn=respond, inputs=text, outputs=[output, raw_prompt, solution])
+    text.submit(fn=respond, inputs=text, outputs=[problem, solution, llm_answer, raw_prompt])
 
     send_button = gr.Button(value="Отправить")
-    send_button.click(fn=respond, inputs=text, outputs=[output, raw_prompt, solution])
+    send_button.click(fn=respond, inputs=text, outputs=[problem, solution, llm_answer, raw_prompt])
 
 embeddings_raw, embeddings_text, embeddings_answer = create_embeddings(test_loco.dict, use_cuda=True)
 
-demo.launch()
+demo.launch(share=True)
--- text_llm.py
+++ text_llm.py
@@ -1,2 +1,27 @@
-def get_reply(text, embeddings):
-    return '-'
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+llm_model = AutoModelForCausalLM.from_pretrained("TheBloke/WizardLM-1.0-Uncensored-CodeLlama-34B-GPTQ", code_revision='gptq-4bit-64g-actorder_True', device_map='cuda')
+llm_tokenizer = AutoTokenizer.from_pretrained("TheBloke/WizardLM-1.0-Uncensored-CodeLlama-34B-GPTQ", code_revision='gptq-4bit-64g-actorder_True', device_map='cuda')
+
+def get_reply(text):
+    model_inputs = llm_tokenizer([text], return_tensors='pt').to('cuda')
+    len_inputs = len(model_inputs['input_ids'])
+    generated_ids = llm_model.generate(**model_inputs,
+                                        max_new_tokens=200,
+                                        do_sample=True,
+                                        temperature=0.1,
+                                        top_p=0.9,
+                                        top_k=20,
+                                        repetition_penalty=1.15,
+                                       )
+    answer_string = llm_tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+    return answer_string
+
+local_test = 0
+if local_test:
+    user_text = 'Tell me something about Antarctica.'
+    llm_prompt = f'A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user\'s questions.\nUSER: {user_text}'
+    llm_prompt = llm_prompt + '\n\nASSISTANT: '
+    answer_string = get_reply(llm_prompt)
+    print(answer_string)
