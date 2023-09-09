6be78da5fa83e79dbb881fb5ac631a7d1bdabfde
Milana <Milana@MacBook-Air.local>
2023-09-09 03:33:56 +0300

    add denoising

--- whisper_speech_recognition.py
+++ whisper_speech_recognition.py
@@ -1,33 +1,33 @@
 import whisper
 import torch
 import torchaudio
-import soundfile
-import numpy as np
+from denoiser import pretrained
+from denoiser.dsp import convert_audio
+import torchaudio.transforms as T
+
 
 def speech_to_text(wav):
-    #wav_2, sr = soundfile.read(wav, dtype='float32')
-    wav_2 = wav[1]
+    audio = wav[1]
     sr = wav[0]
-    print(sr)
-    print(wav_2)
-    print(f'mean: {np.mean(wav_2)}, std: {np.std(wav_2)}, 90per: {np.percentile(wav_2, 90)}')
+
     resampler = torchaudio.transforms.Resample(sr, 16000)
-
-    wav_2 = resampler(torch.Tensor(wav_2))
-    #wav_2 = wav_2 * 32768.0
-    wav_2 = wav_2 / 32768.0 / 32768.0
-    print(wav_2)
-    print(f'mean: {torch.mean(wav_2)}, std: {torch.std(wav_2)}, 90per: {torch.quantile(wav_2, 0.9)}')
-    print(wav_2.dtype)
-
-    #exit()
+    model_denoiser = pretrained.dns64()
     if torch.cuda.is_available():
         model_whisper = whisper.load_model("small", device=torch.device("cuda"))
     else:
         model_whisper = whisper.load_model("tiny", device=torch.device("cpu"))
-    predictions = model_whisper.transcribe(wav_2, language='ru')
-    print(predictions)
-    return predictions['text']
 
-#result = speech_to_text('m.ogg')
-#print(result)
+    audio = resampler(torch.Tensor(audio))
+    # wav_2 = wav_2 * 32768.0
+    audio = audio / 32768.0 / 32768.0
+    audio = convert_audio(
+        audio.unsqueeze(0),
+        model_denoiser.sample_rate,
+        model_denoiser.sample_rate,
+        model_denoiser.chin,
+    )
+    with torch.no_grad():
+        denoised_audio = model_denoiser(audio[None])[0].data.cpu().numpy()
+
+    predictions = model_whisper.transcribe(denoised_audio, language="ru")["text"]
+    return predictions
