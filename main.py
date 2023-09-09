54b0166e71e6b8eb344d359909b51a7b9deb7957
asdgg <a@a.a>
2023-09-09 03:10:32 +0300

    Включение/выключение LLM (верх main.py) Базовая интеграция Whisper

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import torch
 import numpy as np
 
+use_llm = False
+
 # Данные - чтение документа и разбивка на читаемые куски
 import test_loco
 from document_reader import get_text_from_document
@@ -11,8 +13,9 @@ from make_embeddings import create_embeddings, get_embedding
 # Вход - звуковой файл
 from whisper_speech_recognition import speech_to_text
 
-# Языковая модель
-from text_llm import get_reply
+if use_llm:
+    # Языковая модель
+    from text_llm import get_reply
 
 # Озвучка текста
 from tts_model import get_wav_from_text
@@ -20,7 +23,7 @@ from tts_model import get_wav_from_text
 # Gradio main thread
 import gradio as gr
 
-def respond(text):
+def respond(text, to_text=False):
     print(f'user question: {text}')
     embedding = get_embedding(text)
 
@@ -37,21 +40,29 @@ def respond(text):
     # vicuna 1.1 as stated on a model card
     llm_prompt = f'A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user\'s questions.\nUSER: '
     # sub-instruction for specific task
-    llm_prompt = llm_prompt + 'Answer the question using only provided context below. Answer using your own understanding of the context. Be concise and precise. Answer in Russian.\n'
+    #llm_prompt = llm_prompt + 'Answer the question or give the solution to the problem using only provided context below. Answer using your own understanding of the context. Be concise and precise. Answer in Russian.\n'
+    llm_prompt = llm_prompt + 'Answer the question or give the solution to the problem. Answer using your own understanding of the context. Be concise and precise. Answer in Russian.\n'
     # User question
-    llm_prompt = llm_prompt + f'Question: {text}\n'
+    llm_prompt = llm_prompt + f'Запрос пользователя: {text}\n'
     # Retrieval-Augmented Generation, 4 nearest examples from embedding database + solutions
     llm_prompt = llm_prompt + 'Context: '
     for i in range(0, 4):
+        problem_text = str.replace(embeddings_text[sorted_index[i]], "\n", "")
+        solution_text = str.replace(embeddings_answer[sorted_index[i]], "\n", "")
+        story_text = f'Проблема: {problem_text}, Решение: {solution_text}\n'
         #llm_prompt = llm_prompt + 'Issue: ' + embeddings_text[sorted_index[i]] + '.'
         #llm_prompt = llm_prompt + 'Solution: ' + embeddings_answer[sorted_index[i]] + '.\n'
-        llm_prompt = llm_prompt + embeddings_answer[sorted_index[i]]
+        #llm_prompt = llm_prompt + embeddings_answer[sorted_index[i]]
+        llm_prompt = llm_prompt + story_text
 
     # LLM answer
     llm_prompt = str.replace(llm_prompt, '..', '.')
     llm_prompt = llm_prompt + '\nASSISTANT: '
 
-    full_llm_answer = get_reply(llm_prompt)
+    if use_llm:
+        full_llm_answer = get_reply(llm_prompt)
+    else:
+        full_llm_answer = ''
     print(f'llm answer before prune: {full_llm_answer}\n')
 
     #llm_prompt = llm_prompt + str(len(llm_prompt))
@@ -66,15 +77,24 @@ def respond(text):
         solution_text = f'{solution_text}{embeddings_answer[sorted_index[i]]}'
         answer_string = f'{answer_string}{embeddings_text[sorted_index[i]]}'
 
-    return answer_string, solution_text, full_llm_answer, llm_prompt
+    if to_text == False:
+        return answer_string, solution_text, full_llm_answer, llm_prompt
+    else:
+        return answer_string, solution_text, full_llm_answer, llm_prompt, text
+
+def asr_wrapper(wav):
+    whisper_text = speech_to_text(wav)
+    return respond(whisper_text, to_text=True)
 
 with gr.Blocks() as demo:
+    audio_input = gr.Audio(source="microphone", type="numpy")
     text = gr.Textbox(label="Запрос")
     problem = gr.Textbox(label="Проблема")
     solution = gr.Textbox(label="Метод устранения", lines=4)
     llm_answer = gr.Textbox(label="Ответ помощника", lines=7)
     raw_prompt = gr.Textbox(label="Raw Prompt", lines=10)
 
+    audio_input.stop_recording(fn=asr_wrapper, inputs=audio_input, outputs=[problem, solution, llm_answer, raw_prompt, text])
     text.submit(fn=respond, inputs=text, outputs=[problem, solution, llm_answer, raw_prompt])
 
     send_button = gr.Button(value="Отправить")
@@ -82,4 +102,5 @@ with gr.Blocks() as demo:
 
 embeddings_raw, embeddings_text, embeddings_answer = create_embeddings(test_loco.dict, use_cuda=True)
 
-demo.launch(share=True)
+demo.launch()
+#demo.launch(share=True)
--- whisper_speech_recognition.py
+++ whisper_speech_recognition.py
@@ -1,2 +1,33 @@
+import whisper
+import torch
+import torchaudio
+import soundfile
+import numpy as np
+
 def speech_to_text(wav):
-    return "-"
+    #wav_2, sr = soundfile.read(wav, dtype='float32')
+    wav_2 = wav[1]
+    sr = wav[0]
+    print(sr)
+    print(wav_2)
+    print(f'mean: {np.mean(wav_2)}, std: {np.std(wav_2)}, 90per: {np.percentile(wav_2, 90)}')
+    resampler = torchaudio.transforms.Resample(sr, 16000)
+
+    wav_2 = resampler(torch.Tensor(wav_2))
+    #wav_2 = wav_2 * 32768.0
+    wav_2 = wav_2 / 32768.0 / 32768.0
+    print(wav_2)
+    print(f'mean: {torch.mean(wav_2)}, std: {torch.std(wav_2)}, 90per: {torch.quantile(wav_2, 0.9)}')
+    print(wav_2.dtype)
+
+    #exit()
+    if torch.cuda.is_available():
+        model_whisper = whisper.load_model("small", device=torch.device("cuda"))
+    else:
+        model_whisper = whisper.load_model("tiny", device=torch.device("cpu"))
+    predictions = model_whisper.transcribe(wav_2, language='ru')
+    print(predictions)
+    return predictions['text']
+
+#result = speech_to_text('m.ogg')
+#print(result)
